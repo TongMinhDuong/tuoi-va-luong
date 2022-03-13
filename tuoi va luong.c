7a936fd737132357bf4f829b4c0a8f753c7469bf
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 10:38:27 +0700

    Add files via upload

---
+++ tuoi va luong.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int tuoi = 25 ;
+	float luong = 1200.05;
+	
+	printf(" So tuoi cua nhan vien la : %d tuoi\n", tuoi);	
+	printf(" Muc luong cua nhan vien la : %f $", luong);
+
+	return 0;
+}
